77085ba6a496e84636fe11de324f154d2451c781
João Victor Ventura Martins <jvmartinscv@gmail.com>
2025-11-01 00:42:26 -0300

    Altera importacao de fonts
    Alterei a importacao das fontes para um arquivo separado por motivos de organizacao

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,6 @@
-import { Open_Sans } from "next/font/google";
 import "../../styles/globals.css";
+
+import { openSans } from "@/components/layout/fonts";
 
 export default function RootLayout({
   children,
@@ -7,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={"openSans.variable"}>
       <body
         className={`antialiased`}
       >
