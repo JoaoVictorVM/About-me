c52dc56402bca37e7f5b197d0af4b1f4ba5f8cc0
João Victor Ventura Martins <jvmartinscv@gmail.com>
2025-11-21 15:33:14 -0300

    Refatorei hero section
    Fiz modificacoes na hero-section e alterei a forma como ira ser o projeto, fiz a estruturacao inicial da pagina

--- src/components/pages/home/hero-section.tsx
+++ src/components/pages/home/hero-section.tsx
@@ -3,43 +3,24 @@ import { Linkedin, Instagram, Github, Phone, Mail, ArrowUpRight, BriefcaseBusine
 
 export default function HeroSection() {
     return (
-        <section className="flex min-h-screen w-full items-center justify-center bg-[var(--color-bg)] text-white">
-            <div className="flex flex-col items-center w-[90%] max-w-[420px] rounded-2xl bg-white/10 backdrop-blur-xl border border-white/20 p-6 shadow-[0_0_40px_rgba(255,255,255,0.1)]  transition-all duration-300 gap-5">
-                <Image
-                    src="/assets/images/Profile.png"
-                    alt="Imagem de fundo"
-                    width={200}
-                    height={200}
-                    className="rounded-full object-cover border-2 border-[var(--color-light)]"
-                />
-
-                <div className="text-center flex flex-col gap-2">
-                    <h1>Joao Victor Ventura Martins</h1>
-                    <h2 className="text-[var(--color-highlight)]">Fullstack Developer</h2>
+        <section className="flex min-h-screen w-full justify-center bg-[var(--color-bg)] text-white">
+            <div className="flex flex-col gap-5 w-3xl mt-36">
+                <div className="flex flex-col gap-1">
+                    <h1 className="text-3xl font-bold">JOAO VICTOR VENTURA MARTINS</h1>
+                    <p className="text-[var(--color-highlight)] font-bold tracking-[0.3rem]">FULLSTACK DEVELOPER</p>
                 </div>
-
-                <ul className="flex flex-col gap-2">
-                    <a href="">
-                        <li>
-                            Linkedin
-                        </li>
-                    </a>
-                    <a href="">
-                        <li>
-                            Instagram
-                        </li>
-                    </a>
-                    <a href="">
-                        <li>
-                            GitHub
-                        </li>
-                    </a>
-                    <a href="">
-                        <li>
-                            Portfolio
-                        </li>
-                    </a>
-                </ul>
+                <div className="flex gap-5">
+                    <div className="flex flex-col gap-1">
+                        <h2>Quem sou eu?</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique adipisci magni non modi corporis sequi inventore autem, ipsa reiciendis cum culpa repellat eius fuga doloremque nam aperiam libero officia tenetur!</p>
+                    </div>
+                    <img
+                        src="/assets/images/Profile.png"
+                        alt=""
+                        className="w-3xs h-3xs"
+                    />
+                </div>
+                <div className="bg-[var(--color-highlight)] h-[1px] mt-10 mb-10"></div>
             </div>
         </section>
     )
